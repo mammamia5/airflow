af486411bcdf2290f53ffa9acffe75bc8b178422
Kimwonjoon <kimpass189@gmail.com>
2024-08-27 11:47:32 +0900

    nice

--- dags/chat_dags.py
+++ dags/chat_dags.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from textwrap import dedent
+import time
 
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
@@ -26,17 +27,23 @@ def save_kafka_to_parquet():
         'mammamia3',
         bootstrap_servers=["ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092"],
         auto_offset_reset="earliest",
-        enable_auto_commit=True,
-        group_id='chat_group1',
-        value_deserializer=lambda x: loads(x.decode('utf-8'))
+#        enable_auto_commit=True,
+#        group_id='chat_group1',
+        value_deserializer=lambda x: loads(x.decode('utf-8')),
+        consumer_timeout_ms=1000,
     )
 
     # Kafka 메시지를 받아와서 리스트에 저장
+    #start_time = time.time()
+    #max_duration = 1 * 60
     for m in consumer:
         data = m.value
         messages.append(data)
+
+        #if time.time() - start_time > max_duration:
+            #break
     # Kafka Consumer 종료
-    consumer.close()
+    #consumer.close()
 
         # 리스트를 Parquet 파일로 저장
     if messages:
@@ -44,7 +51,8 @@ def save_kafka_to_parquet():
         file_name = f"~/data/chat_data/chat_mam.parquet"
         df.to_parquet(file_name, index=False)
         print(f"Parquet 파일로 저장 완료: {file_name}")
-
+        return True
+    return True
 # Airflow DAG 정의
 default_args = {
     'owner': 'airflow',
@@ -54,12 +62,21 @@ default_args = {
 }
 
 with DAG(
-    dag_id='kafka_to_parquet_dag',
-    default_args=default_args,
-    schedule_interval='@hourly',  # 매 시간 실행
-    catchup=False,
+    'chat_parquet',
+    default_args={
+        'depends_on_past': False,
+        'retries': 1,
+        'retry_delay': timedelta(seconds=3),
+    },
+    description='hello world DAG',
+    schedule="10 2 * * *",
+    start_date=datetime(2024, 8, 26),
+    #end_date=datetime(2016,1,1),
+    catchup=True,
+    tags=["kafka", "chat"],
+#    max_active_runs=1,  # 동시에 실행될 수 있는 최대 DAG 인스턴스 수
+#    max_active_tasks=3,  # 동시에 실행될 수 있는 최대 태스크 수
 ) as dag:
-    
     # PythonOperator로 Kafka 데이터를 Parquet로 저장하는 작업 실행
     save_parquet_task = PythonOperator(
         task_id='save_parquet',
